33f9e76309c0d2b161f305bcf408236edeacfe1b
unknown <javiersolis921@hotmail.com>
2023-08-01 11:36:31 -0300

    Se prepara el etorno para deploy

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -5,12 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsComponent } from './components/components.component';
 import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaComponent } from './components/acerca/acerca.component';
+import { EduComponent } from './components/edu/edu.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentsComponent,
-    HeaderComponent
+    HeaderComponent,
+    BannerComponent,
+    AcercaComponent,
+    EduComponent,
+    ProyectoComponent,
+    ExperienciaComponent,
+    SkillsComponent,
+    ContactoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
